b9b3f74541b758a9fb217eea95147e85c8f6840b
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2024-11-27 16:37:03 +0530

    Update Hospital.model.js

--- Hospital/Hospital.model.js
+++ Hospital/Hospital.model.js
@@ -1,12 +1,27 @@
 import mongoose from 'mongoose';
+import { ObjectId } from './../../../node_modules/bson/src/objectid';
+import { hospital } from './Hospital.model';
 
-const HospitalSchema = new mongoose.Schema({
 
-
+const DoctorSchema = new mongoose.Schema({
+        name:{
+            type:String,
+            required:true
+        },
+        qualification:{
+            type:String,
+            required:true
+        },
+        experience:{
+            type:Number,
+            default:1
+        },
+        Hospitals:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"hospital"
+        }
 
 },{timestamps:true})
 
 
-
-export const hospital = mongoose.model("hospital",HospitalSchema)
-
+export const doctor = mongoose.model("doctor",DoctorSchema)
