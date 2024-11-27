5ee8a7da641a585721fee912c965706d369abd69
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2024-11-27 16:43:27 +0530

    MedicalRecords.model.js

--- Hospital/MedicalRecords.model.js
+++ Hospital/MedicalRecords.model.js
@@ -1,7 +1,39 @@
 import mongoose from 'mongoose';
+import { ObjectId } from './../../../node_modules/bson/src/objectid';
 
 
 const MedicalRecordSchema = new mongoose.Schema({
+
+    patient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"patient",
+        required:true
+    },
+    doctor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'doctor',
+        required:true
+    },
+    hospital:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'hospital',
+        required:true
+    },
+    diagnosis:{
+        type:String,
+        required:true
+    },
+    treatment:{
+        type:String,
+        required:true,
+    },
+    recorddate:{
+        type:Date,
+        default:Date.now
+    },
+    prescription:{
+        type:String,
+    }
 
 },{})
 
